5afe11db266003f075450b372e39c7f9e4124ed9
wt_dev <greyHairChooseLife@gmail.com>
2021-08-09 09:57:48 +0900

    user router 구성 변경 - john

--- src/routers/user/index.js
+++ src/routers/user/index.js
@@ -2,9 +2,19 @@ const express = require('express');
 const userCtrl = require('./user.ctrl.js');
 const userRouter = express.Router();
 
-userRouter.post('/', userCtrl.create);
-userRouter.get('/:id', userCtrl.read);
-userRouter.post('/:id', userCtrl.update);
-userRouter.delete('/:id', userCtrl.delete);
+// todo
+// 1. 회원가입
+// 2. 로그인 
+// 3. 로그아웃
+// 4. 회원 정보 수정
+// 5. 회원탈퇴
+
+// 로그인 체크는 미들웨어로 구현해야함
+
+userRouter.post('/register', userCtrl.register);
+userRouter.post('/login', userCtrl.login);
+userRouter.post('/logout', userCtrl.logout);
+userRouter.post('/edit', userCtrl.edit);
+userRouter.post('/delete', userCtrl.delete);
 
 module.exports = userRouter;
--- src/routers/user/user.ctrl.js
+++ src/routers/user/user.ctrl.js
@@ -1,20 +1,43 @@
-exports.create = (req, res)=>{
-    res.send('create');
-    console.log('create user');
+// 회원 관리에 jwt 토큰을 사용해야 할 것 같습니다.
+exports.register = (req, res)=>{ // 회원 가입
+    // todo
+    // 1. email, password 값 존재 확인
+    // 2. DB에 이메일이 이미 존재하는 검사
+    // 3. 없다면 이메일 인증시작
+    // 4. 이메일과 비밀번호를 db에 저장
+    // 5. 다시 로그인 요청 혹은 자동 로그인
+    // 6. / 로 리다이렉션,
+    console.log('register user');
     console.log(req);
+    res.send('register');
 }
-exports.read = (req, res)=>{
-    res.send('read');
-    console.log('read user');
+exports.edit = (req, res)=>{
+    // 회원 정보 수정 구현
+    console.log('login user');
     console.log(req);
+    res.send('login');
 }
-exports.update = (req, res)=>{
-    res.send('update');
-    console.log('update user');
+exports.login = (req, res)=>{ // 로그인, 세션에 로그인 정보를 저장
+    // todo
+    // 1. email, password 값 존재 확인
+    // 2. DB에 계정이 존재하는 검사
+    // 3. 계정이 존재한다면 password와 함께 다시 검색
+    // 4. 로그인 완료 토큰 발급
+    // 5. / 로 리다이렉션
+    console.log('login user');
     console.log(req);
+    res.send('login');
 }
-exports.delete = (req, res)=>{
-    res.send('delete');
-    console.log('delete user');
+exports.logout = (req, res)=>{ // 로그아웃, 세션 파괴
+    // todo
+    // 1. email, password 값 존재 확인
+    // 2. / 로 리다이렉션
+    console.log('logout user');
     console.log(req);
+    res.send('logout');
+}
+exports.delete = (req, res)=>{ // 계정 삭제
+    console.log(req);
+    console.log('delete account');
+    res.send('delete account');
 }
